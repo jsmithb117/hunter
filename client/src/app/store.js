ddd3c8a9c4efa6e127a3822bce8ff3179e708417
jsmithb117 <jsmithb117@gmail.com>
2022-01-21 10:01:29 -0800

    Removes counter, adds board, form, user to configureStore

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import boardReducer from '../features/board/boardSlice';
+import formReducer from '../features/form/formSlice';
+import userReducer from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    board: boardReducer,
+    form: formReducer,
+    user: userReducer,
   },
 });
