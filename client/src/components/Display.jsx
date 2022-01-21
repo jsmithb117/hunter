ce9289387fa01410ad0d66fac22c5b5211546a91
jsmithb117 <jsmithb117@gmail.com>
2022-01-21 10:23:01 -0800

    Renders Display component

---
+++ client/src/components/Display.jsx
@@ -0,0 +1,23 @@
+import Health from './Health';
+import MinesCount from './MinesCount';
+import {
+  selectCurrentHealth,
+  selectTotalHealth,
+} from '../features/user/userSlice';
+
+const Display = () => {
+  const currentHealth = selectCurrentHealth();
+  const totalHealth = selectTotalHealth();
+
+  return (
+    <div className="display">
+      <Health
+        currentHealth={currentHealth}
+        totalHealth={totalHealth}
+      />
+      <MinesCount />
+    </div>
+  );
+};
+
+export default Display;
