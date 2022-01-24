5ed297bd47148aea07e933c17dfca394f1ea983c
jsmithb117 <jsmithb117@gmail.com>
2022-01-24 14:02:30 -0800

    Moves initialState

---
+++ client/src/features/initialState.js
@@ -0,0 +1,48 @@
+import { EmptyPiece } from './board/PieceCreators';
+
+export const difficulty = {
+  beginner: {
+    label: 'beginner',
+    length: 8,
+    width: 8,
+    mines: 8,
+    vaccines: 3,
+    lockdowns: 3,
+  },
+  intermediate: {
+    label: 'intermediate',
+    length: 13,
+    width: 15,
+    mines: 40,
+    vaccines: 3,
+    lockdowns: 3,
+  },
+  expert: {
+    label: 'expert',
+    length: 16,
+    width: 30,
+    mines: 99,
+    vaccines: 1,
+    lockdowns: 5,
+  },
+};
+
+const newPiece = new EmptyPiece();
+const initialState = {
+  board: {
+    piecesMarkedAsMine: 0,
+    rows: [[ newPiece ]],
+    win: false,
+    loss: false,
+  },
+  display: {
+    minesDisplay: 0,
+    totalHealth: 3,
+    currentHealth: 3,
+    difficulty: difficulty.beginner,
+    paused: false,
+    highScores: [],
+  },
+};
+
+export default initialState;
