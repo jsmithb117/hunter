b899e3e7436c4e7c86e1960eaf99bc452ff5371b
jsmithb117 <jsmithb117@gmail.com>
2022-01-24 14:00:29 -0800

    Changes form to display

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import boardReducer from '../features/board/boardSlice';
-import formReducer from '../features/form/formSlice';
-import userReducer from '../features/user/userSlice';
+import displayReducer from '../features/display/displaySlice';
 
 export const store = configureStore({
   reducer: {
     board: boardReducer,
-    form: formReducer,
-    user: userReducer,
+    display: displayReducer,
   },
 });
