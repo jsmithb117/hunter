163f2d3484b6b85e3fee28bca65f3786d92485c1
jsmithb117 <jsmithb117@gmail.com>
2022-01-24 13:34:20 -0800

    Changes state.form to state.display. Moves Display and Rows to App. Adds Error Boundaries.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,67 +1,86 @@
-import React, { useState, useEffect } from 'react';
-
-import './App.css';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import Home from './components/Home';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Rows from "./features/board/Rows";
+import Display from "./features/display/Display";
+import ErrorBoundary from "./ErrorBoundary";
 
 import {
   createNewBoard,
   selectWin,
   selectLoss,
-} from './features/board/boardSlice';
-import { selectDifficulty } from './features/form/formSlice';
+} from "./features/board/boardSlice";
+import { selectDifficulty } from "./features/display/displaySlice";
 
 // debug
-import pieceCounter from './debug/pieceCounter';
-import { selectBoard } from './features/board/boardSlice';
+import { selectBoard } from "./features/board/boardSlice";
 // /debug
 
 function App() {
-  const [boardColor, setBoardColor] = useState('#fff');
-  const win = selectWin();
-  const loss = selectLoss();
-  const difficulty = selectDifficulty();
+  const [boardColor, setBoardColor] = useState("#fff");
+  const win = useSelector(selectWin);
+  const loss = useSelector(selectLoss);
+  const difficulty = useSelector(selectDifficulty);
+  const dispatch = useDispatch();
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
 
   // debug
-  const board = selectBoard();
+  // const board = useSelector(selectBoard);
+  // let minesCount = 0;
+  // let vaccinesCount = 0;
+  // let lockdownsCount = 0;
+  // board.forEach((row, rowIndex) => {
+  //   row.forEach((piece, colIndex) => {
+  //     if (piece.isMine) {
+  //       minesCount++;
+  //     }
+  //     if (piece.isVaccine) {
+  //       vaccinesCount++;
+  //     }
+  //     if (piece.isLockdown) {
+  //       lockdownsCount++;
+  //     }
+  //   });
+  // });
+  // console.log("pieceCount: ", { minesCount, vaccinesCount, lockdownsCount });
   // /debug
 
   useEffect(() => {
-    createNewBoard(difficulty);
-
-    // debug
-    console.log(pieceCounter(board));
-    // also remove board dependency
-    // /debug
-
-  }, [difficulty, board]);
+    dispatch(createNewBoard(difficulty));
+  }, [difficulty, dispatch]);
 
   useEffect(() => {
     if (win) {
       // TODO: use color names
-      setBoardColor('#2ecc71');
+      setBoardColor("#2ecc71");
     }
     if (loss) {
       // TODO: use color names
-      setBoardColor('#e74c3c');
+      setBoardColor("#e74c3c");
     }
   }, [win, loss, setBoardColor]);
 
   return (
     <div className="covid-hunter">
-    <HelmetProvider>
-      <Home />
-      <Helmet>
-        <style type='text/css'>
-          {`
+      <HelmetProvider>
+        <ErrorBoundary>
+          <Display />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Rows />
+        </ErrorBoundary>
+        <Helmet>
+          <style type="text/css">
+            {`
           body {
             background-color : ${boardColor}
           }
           `}
-        </style>
-      </Helmet>
-    </HelmetProvider>
-  </div>
+          </style>
+        </Helmet>
+      </HelmetProvider>
+    </div>
   );
 }
 
