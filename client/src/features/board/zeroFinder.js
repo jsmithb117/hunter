33223847519c775ff92b4f0fc890c8a6e7892e46
jsmithb117 <jsmithb117@gmail.com>
2022-01-26 13:02:21 -0800

    Refactors to track and return vaccinesCount

--- client/src/features/board/zeroFinder.js
+++ client/src/features/board/zeroFinder.js
@@ -1,17 +1,26 @@
-// if piece is a mine, noop
+// if piece is already uncovered, noop
 // if piece is adjacent to a mine, uncovers that piece
-// if piece is not adjacent to a mine, recursively uncovers adjacent pieces
-const zeroFinder = (rows, row, col) => {
+// if piece is not adjacent to a mine, uncovers that piece and recursively uncovers adjacent pieces
+let zeroFinder = (rows, row, col) => {
   let uncoveredPiecesCount = 0;
-  if (rows[row][col].isMarkedAsMine) {
-    return { uncoveredPiecesCount, rows };
+  let vaccinesCount = 0;
+  const piece = rows[row][col];
+
+  const { covered, isMarkedAsMine, adjacentMines, isVaccine } = piece;
+  if (!covered) {
+    return { rows, uncoveredPiecesCount, vaccinesCount }
   }
-
-  rows[row][col].covered = false;
+  if (isMarkedAsMine) {
+    return { rows, uncoveredPiecesCount, vaccinesCount };
+  }
+  rows[row][col] = { ...piece, covered: false };
   uncoveredPiecesCount++;
+  if (isVaccine) {
+    vaccinesCount++;
+  }
+  if (adjacentMines !== 0) {
+    return { rows, uncoveredPiecesCount, vaccinesCount };
 
-  if (rows[row][col].adjacentMines !== 0) {
-    return { uncoveredPiecesCount, rows };
   }
   const prevRow = row - 1;
   const nextRow = row + 1;
@@ -30,43 +39,52 @@ const zeroFinder = (rows, row, col) => {
     const newBoard = zeroFinder(rows, prevRow, prevCol);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
   if (UU && UU.covered) {
     const newBoard = zeroFinder(rows, prevRow, col);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
   if (UR && UR.covered) {
     const newBoard = zeroFinder(rows, prevRow, nextCol);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
   if (LL && LL.covered) {
     const newBoard = zeroFinder(rows, row, prevCol);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
   if (RR && RR.covered) {
     const newBoard = zeroFinder(rows, row, nextCol);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
   if (DL && DL.covered) {
     const newBoard = zeroFinder(rows, nextRow, prevCol);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
   if (DD && DD.covered) {
     const newBoard = zeroFinder(rows, nextRow, col);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
   if (DR && DR.covered) {
     const newBoard = zeroFinder(rows, nextRow, nextCol);
     rows = newBoard.rows;
     uncoveredPiecesCount += newBoard.uncoveredPiecesCount;
+    vaccinesCount += newBoard.vaccinesCount;
   }
-  return { rows, uncoveredPiecesCount };
+  return { rows, uncoveredPiecesCount, vaccinesCount };
+
 };
 
 export default zeroFinder;
