c3f1fc291c40206b68d7ab4e0e723fa1efc3a4f1
jsmithb117 <jsmithb117@gmail.com>
2022-01-27 10:32:46 -0800

    Adds timeReducer to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import boardReducer from '../features/board/boardSlice';
 import countReducer from '../features/count/countSlice';
 import difficultyReducer from '../features/difficulty/difficultySlice';
+import timeReducer from '../features/time/timeSlice';
 
 const store = configureStore({
   reducer: {
     board: boardReducer,
     count: countReducer,
     difficulty: difficultyReducer,
+    time: timeReducer,
   },
 });
 
