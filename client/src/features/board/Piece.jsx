5f8473fedbb561b3af6085a6316c303e2bf75ecf
jsmithb117 <jsmithb117@gmail.com>
2022-01-27 11:00:07 -0800

    Lints component

--- client/src/features/board/Piece.jsx
+++ client/src/features/board/Piece.jsx
@@ -1,17 +1,17 @@
-import { useDispatch, useSelector } from 'react-redux';
-import CoronavirusSharpIcon from '@mui/icons-material/CoronavirusSharp';
-import MasksSharpIcon from '@mui/icons-material/MasksSharp';
-import LockSharpIcon from '@mui/icons-material/LockSharp';
+import { useDispatch, useSelector } from "react-redux";
+import CoronavirusSharpIcon from "@mui/icons-material/CoronavirusSharp";
+import MasksSharpIcon from "@mui/icons-material/MasksSharp";
+import LockSharpIcon from "@mui/icons-material/LockSharp";
 import Button from "@mui/material/Button";
-import buttonTextColor from './buttonTextColor';
+import buttonTextColor from "./buttonTextColor";
 import { indigo, red } from "@mui/material/colors";
-import InjectionSVG from './InjectionSVG';
-import zeroFinder from './zeroFinder';
-import styles from './Piece.module.css';
+import InjectionSVG from "./InjectionSVG";
+import zeroFinder from "./zeroFinder";
+import styles from "./Piece.module.css";
 
 import { toggleMarked, selectRows } from "./boardSlice";
 
-import { uncover, selectLoss } from '../count/countSlice';
+import { uncover, selectLoss } from "../count/countSlice";
 
 const Piece = (props) => {
   const { piece, color } = props;
@@ -20,7 +20,7 @@ const Piece = (props) => {
   const rows = useSelector(selectRows);
   const dispatch = useDispatch();
   const loss = useSelector(selectLoss);
-  const buttonColor = isMine && !covered ? red[900] : indigo[100]
+  const buttonColor = isMine && !covered ? red[900] : indigo[100];
 
   const icon = isMarkedAsMine ? (
     <CoronavirusSharpIcon
@@ -45,7 +45,7 @@ const Piece = (props) => {
     <div className={styles.lock}>
       <LockSharpIcon
         sx={{
-          zIndex: '1',
+          zIndex: "1",
         }}
       />
       <span className={styles.number}>{piece.adjacentMines}</span>
@@ -60,7 +60,9 @@ const Piece = (props) => {
       return;
     }
     const rowsCopy = rows.map((row) => row.slice());
-    dispatch(uncover({ piece, zeroFinderResults: zeroFinder(rowsCopy, row, col) }));
+    dispatch(
+      uncover({ piece, zeroFinderResults: zeroFinder(rowsCopy, row, col) })
+    );
   };
   const rightClickHandler = () => {
     if (!loss) {
@@ -68,21 +70,21 @@ const Piece = (props) => {
     }
   };
 
-    return (
-      <Button
-        onClick={() => leftClickHandler()}
-        onContextMenu={() => rightClickHandler()}
-        color={color}
-        sx={{
-          width: '13.68%',
-          color: renderColor,
-          border: '1px solid black',
-          backgroundColor: buttonColor,
-        }}
-      >
-        {icon}
-      </Button>
-    );
+  return (
+    <Button
+      onClick={() => leftClickHandler()}
+      onContextMenu={() => rightClickHandler()}
+      color={color}
+      sx={{
+        width: "13.68%",
+        color: renderColor,
+        border: "1px solid black",
+        backgroundColor: buttonColor,
+      }}
+    >
+      {icon}
+    </Button>
+  );
 };
 
 export default Piece;
